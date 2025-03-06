d315093f45da052f9c2fd0842b6a402f07a28661
TheMostHigh <tallestlegacy@gmail.com>
2025-03-07 00:42:20 +0300

    feature: Sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<iframe
+	class="h-full w-full"
+	src="https://www.youtube.com/embed/EewB7xHHIvE"
+	title="Kishi Bashi - I Am The Antichrist To You (Official Video)"
+	frameborder="0"
+	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+	referrerpolicy="strict-origin-when-cross-origin"
+	allowfullscreen
+></iframe>
